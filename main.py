091700a6a64f635856c4a1f9fa22c9780ee937fe
Jakariya Khan <jakariya012006@gmail.com>
2025-10-01 17:07:02 +0530

    Add a registration interface

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ class NLP:
 
         label4 = Label(self.root, text="Not a member?", bg="#d9d9d9", font=('Verdana', 10))
         label4.pack(pady=(10, 0))
-        register_btn = Button(self.root, text="Register here", bg="blue", fg="white", width=20)
+        register_btn = Button(self.root, text="Register here", bg="blue", fg="white", width=20,command=self.register_gui)
         register_btn.pack(pady=(5, 10), ipady=4)
 
     def login(self):
@@ -43,6 +43,34 @@ class NLP:
             messagebox.showinfo("Login", f"Welcome, {email}!")
         else:
             messagebox.showwarning("Login Failed", "Please enter both email and password.")
+
+    def register_gui(self):
+        # self.clear()
+        heading = Label(self.root, text="NLP App", fg="black", bg="#d9d9d9", font=('Verdana', 20, 'bold'))
+        heading.pack(pady=(20, 10))
+
+        label0 = Label(self.root, text="Enter your name:", bg="#d9d9d9", font=('Verdana', 10))
+        label0.pack(pady=(10, 0))
+        self.name_input = Entry(self.root, width=40, fg="black")
+        self.name_input.pack(pady=(5, 10), ipady=4)
+
+        label1 = Label(self.root, text="Enter Email:", bg="#d9d9d9", font=('Verdana', 10))
+        label1.pack(pady=(10, 0))
+        self.email_input = Entry(self.root, width=40, fg="black")
+        self.email_input.pack(pady=(5, 10), ipady=4)
+
+        label2 = Label(self.root, text="Enter Password:", bg="#d9d9d9", font=('Verdana', 10))
+        label2.pack(pady=(10, 0))
+        self.password_input = Entry(self.root, width=40, fg="black", show="*")
+        self.password_input.pack(pady=(5, 10), ipady=4)
+
+        register_btn = Button(self.root, text="Register", bg="green", fg="white", width=20)
+        register_btn.pack(pady=(10, 10), ipady=4)
+
+        label4 = Label(self.root, text="Already a member?", bg="#d9d9d9", font=('Verdana', 10))
+        label4.pack(pady=(10, 0))
+        login_btn = Button(self.root, text="Login here", bg="blue", fg="white", width=20, command=self.login_gui)
+        login_btn.pack(pady=(5, 10), ipady=4)
 
 
 nlp=NLP()
